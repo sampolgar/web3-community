c551859bcdb1854f1129f0589625daa82dc494da
Sam Polgar <sampolgar@gmail.com>
2023-02-07 14:33:53 +1100

    updating to hardhat deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,27 @@
-import { ethers, run, network } from 'hardhat';
-import '@nomiclabs/hardhat-etherscan';
+// // import { ethers, run, network } from 'hardhat';
+// import deployments } from "hardhat"
+// import "@nomiclabs/hardhat-etherscan"
 
-async function main() {
-  const Community = await ethers.getContractFactory('Community');
-  const community = await Community.deploy();
-  await community.deployed();
-  const deployedTransaction = await community.deployTransaction;
-  await deployedTransaction.wait(6);
+// async function main() {
+//   const Community = await ethers.getContractFactory("Community")
+//   const community = await Community.deploy()
+//   await community.deployed()
+//   const deployedTransaction = await community.deployTransaction
+//   await deployedTransaction.wait(6)
 
-  console.log(`community deployed at ${community.address}`);
+//   console.log(`community deployed at ${community.address}`)
 
-  //verify on etherscan only if it uses the network with chainid 5. wait 6 block confirmations before verifying
-  if (network.config.chainId === 5) {
-    await run('verify:verify', {
-      address: community.address,
-      contract: 'contracts/Community.sol:Community',
-      constructorArgs: [],
-    });
-  }
-}
+//   //verify on etherscan only if it uses the network with chainid 5. wait 6 block confirmations before verifying
+//   if (network.config.chainId === 5) {
+//     await run("verify:verify", {
+//       address: community.address,
+//       contract: "contracts/Community.sol:Community",
+//       constructorArgs: [],
+//     })
+//   }
+// }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// main().catch((error) => {
+//   console.error(error)
+//   process.exitCode = 1
+// })
