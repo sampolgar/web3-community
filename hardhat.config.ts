6a2b2b6b953a39b85002315085f4737388bb3620
Sam Polgar <sampolgar@gmail.com>
2023-02-08 16:33:01 +1100

    changing from deploy.ts to hardhat deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,19 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-import 'dotenv/config';
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
+import "hardhat-deploy"
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || '';
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const GOERLI_RPC_URL =
   process.env.GOERLI_RPC_URL ||
-  'https://eth-mainnet.alchemyapi.io/v2/your-api-key';
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
-  '0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a';
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.17',
+  solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 31337,
@@ -27,6 +28,6 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
-};
+}
 
-export default config;
+export default config
